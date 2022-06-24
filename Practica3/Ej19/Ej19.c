f105d35bd86304da9b4e8f374dab158c3776b54c
101 pesos <83673903+74pesos@users.noreply.github.com>
2022-06-24 12:43:14 -0300

    Update Ej19.c

--- Practica3/Ej19/Ej19.c
+++ Practica3/Ej19/Ej19.c
@@ -5,16 +5,8 @@ int main(int argc,char *argv[])
 	if(argc>1)
 	{
 		char str[1000];
-		printf("Ingresar string por teclado\n");
-		fgets(str,1000,stdin);
-		str[strlen(str)-1]='\0';
-		//for(int i=0;i<strlen(str);i++)if(str[i]=='\n') str[i]='\0';
-		while(strcmp(str,argv[1])!=0)
-		{
-			fgets(str,1000,stdin);
-			str[strlen(str)-1]='\0';
-			//for(int i=0;i<strlen(str);i++)if(str[i]=='\n') str[i]='\0';
-		}
+		printf("Ingresar string por teclado\n");;
+		while((strstr(str,argv[1])==0)&&(fgets(str,1000,stdin)!=NULL));
 		return 0;
 	}
 	else
